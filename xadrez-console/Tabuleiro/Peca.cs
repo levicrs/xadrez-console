3f319de7bd410105e76f399eac2bc1e1df9f47d7
Levi Serrano <leviserrano@outlook.pt>
2021-06-05 17:01:20 +0100

    King and Tower movement

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -1,5 +1,5 @@
 ﻿namespace tabuleiro {
-    class Peca {
+    abstract class Peca {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qtdMovimentos { get; protected set; }
@@ -15,6 +15,9 @@
         public void incrementaQtMovimentos() {
             this.qtdMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
+        
 
     }
 }
