cdd78ad68f871c1ffb37447437fbc8a656311bcf
Levi Serrano <leviserrano@outlook.pt>
2021-06-05 15:32:05 +0100

    Color added to table

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -6,12 +6,12 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 0));
 
-            Console.ReadLine();
+            Tela.imprimirTabuleiro(tab);
 
         }
     }
