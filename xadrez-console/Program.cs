4494d4bfdc328f071ea0f5635c5c159e50e255a8
Levi Serrano <leviserrano@outlook.pt>
2021-06-05 13:45:59 +0100

    Add class posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using Tabuleiro;
 
-namespace xadrez_console
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: "+ P);
         }
     }
 }
