d4ae60c02405b582a733cd323d1111e6fda07ac6
Levi Serrano <leviserrano@outlook.pt>
2021-06-05 14:07:37 +0100

    Correcao commit anterior

--- xadrez-console/Tabuleiro/Cor.cs
+++ xadrez-console/Tabuleiro/Cor.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace tabuleiro {
+    enum Cor {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja,
+        Rosa
 
-namespace xadrez_console.Tabuleiro {
-    class Cor {
     }
 }
